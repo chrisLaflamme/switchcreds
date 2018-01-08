ff78a06f0854afb554e44192c0fab0127229ca61
Chris Laflamme <claflamme@vistaprint.com>
2018-01-08 16:36:13 -0500

    switchcreds cmd works - running $ switchcreds and $ switchcreds -h now returns something real

--- lib/switchcreds.rb
+++ lib/switchcreds.rb
@@ -1,5 +1,51 @@
 require "switchcreds/version"
 
 module SwitchCreds
-  # Your code goes here...
+  def get_creds
+      dir = Dir.entries("/Users/#{$user}/.aws")
+      creds = []
+      dir.each do |f|
+        if f.length > 11 && f[0,12] == "credentials_"
+          creds.push(f[12, f.length])
+        end
+      end
+      creds
+    end
+
+  def switch_creds
+    creds = get_creds()
+    puts "Switch to wat?\n"
+
+    i = 0
+    creds.each do |o|
+      puts "#{i}: #{o}"
+      i += 1
+    end
+
+    selection = STDIN.gets.chomp.to_i
+    selection = creds[selection]
+
+    IO.copy_stream("/Users/#{$user}/.aws/credentials_#{selection}", "/Users/#{$user}/.aws/credentials")
+
+    # this is probably a bad idea
+    # File.open("/Users/#{$user}/.aws/credentials").each do |line|
+    #   puts line
+    # end
+    32.times { print "*".colorize(:green) }
+    puts "\n* Using #{selection.upcase!.colorize(:red)} creds now *\n"
+    32.times { print "*".colorize(:green) }
+    puts "\n"
+  end
+
+  def which_creds
+    creds = get_creds
+    creds.each do |cred|
+      if FileUtils.compare_file("/Users/#{$user}/.aws/credentials", "/Users/#{$user}/.aws/credentials_#{cred}")
+        32.times { print "*".colorize(:green) }
+        puts "\n* You're using #{cred.upcase!.colorize(:red)} creds.*\n"
+        32.times { print "*".colorize(:green) }
+        puts "\n"
+      end
+    end
+  end
 end
